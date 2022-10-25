a17bdcad604505aaf176b2d666f3504c7726eef3
alibokhamseen <91407564+alibokhamseen@users.noreply.github.com>
2022-10-25 15:52:12 -0500

    Update UPMASK.R

--- UPMASK.R
+++ UPMASK.R
@@ -1,19 +1,13 @@
 install.packages("UPMASK") # use this command to download the package
 library('UPMASK')
 
-# In the end you will find the gaia query I used.
-# If you use it, you won't need to change columns' numbers. Just make sure to
-# change the location and download in .csv.
-
-# Use ctrl+shift+c to comment/uncomment
+# You won't need to change columns' numbers if you use my gaia query.
 
 # I used .csv for input and output
-
-
-inputFile = "C:\\Users\\aliii\\Downloads\\Berkeley39.csv" # Input file location
+inputFile = "C:\\Users\\ali\\Downloads\\Berkeley39.csv" # Input file location\\name
 
 # Output file location\\name
-outputFile = "C:\\Users\\aliii\\Documents\\2022_Fall\\R\\Berkeley39_k_25.csv"
+outputFile = "C:\\Users\\ali\\Documents\\2022_Fall\\R\\Berkeley39_k_25.csv"
 
 posIdx=c(2,3)                 # ra and dec columns (columns start from 1)
 photIdx=c(4,6,8,10,13,16)     # clustering parameters' columns in inputFile 
@@ -36,33 +30,3 @@ upmaskRes <- UPMASKdata(ocData, posIdx, photIdx, PhotErrIdx, nRuns=120,
 # create an output file
 write.csv(upmaskRes,outputFile, row.names = TRUE)
 # last column include probabilities
-
-
-# my gaia query
-
-# SELECT gaia_source.source_id,gaia_source.ra,gaia_source.dec,gaia_source.parallax,gaia_source.parallax_error,gaia_source.pmra,gaia_source.pmra_error,gaia_source.pmdec,gaia_source.pmdec_error,gaia_source.phot_g_mean_flux,gaia_source.phot_g_mean_flux_error,gaia_source.phot_g_mean_mag,gaia_source.phot_bp_mean_flux,gaia_source.phot_bp_mean_flux_error,gaia_source.phot_bp_mean_mag,gaia_source.phot_rp_mean_flux,gaia_source.phot_rp_mean_flux_error,gaia_source.phot_rp_mean_mag,gaia_source.bp_rp,gaia_source.bp_g,gaia_source.g_rp
-# FROM gaiadr3.gaia_source 
-# WHERE
-# (phot_g_mean_flux is not null)
-# and
-# (phot_bp_mean_flux is not null)
-# and
-# (phot_rp_mean_flux is not null)
-# and
-# (parallax_error is not null)
-# and
-# (parallax is not null)
-# and
-# (pmra is not null)
-# and
-# (pmdec is not null)
-# and
-# 
-# CONTAINS(
-#   POINT('ICRS',gaiadr3.gaia_source.ra,gaiadr3.gaia_source.dec),
-#   CIRCLE(
-#     'ICRS',
-#     COORD1(EPOCH_PROP_POS(116.70199999999998,-4.665,.2010,-1.7300,-1.6450,59.8900,2000,2016.0)),
-#     COORD2(EPOCH_PROP_POS(116.70199999999998,-4.665,.2010,-1.7300,-1.6450,59.8900,2000,2016.0)),
-#     0.11666666666666667)
-# )=1
